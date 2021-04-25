6e35a8c730c77e0c7ce0fd62e881256cc76d163d
waldeir <waldeir@yandex.com>
2021-04-25 13:01:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Rastreamento Correios
+
+Esse script simples serve para rastrear uma encomenda usando seu código de
+rastreamento.
+
+O comando pode ser usado com essa sintaxe:
+```bash
+python correios.py CODIGO_DE_RASTREAMENTO
+```
+
+Ele vai imprimir o caminho que o objeto fez até sua última localização, igual
+aparece no site dos correios. O trabalho é feito usando essa
+[API][correiosAPI].
+
+
+[correiosAPI]:https://github.com/chipytux/correiosApi 
---
+++ correios.py
@@ -0,0 +1,23 @@
+#!/bin/python
+import requests
+import sys
+
+trackingCode=str(sys.argv[1])
+
+url = "https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=" + trackingCode
+payload = {}
+headers= {}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+jason = response.json()
+
+events = jason['eventos']
+
+for i in events:
+    print(i['local'])
+    print(i['data'])
+    print(i['hora'])
+    print("Status: ", i['status'])
+    print("Substatus: ", i['subStatus'])
+    print(' ')
